3ca87f6e28476c42839e584f441c4c1de71fd100
Steffen Gebert <steffen.gebert@typo3.org>
2013-02-17 20:56:21 +0100

    Remove "build-essential" dependency

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,5 @@ description      "Installs/Configures redmine"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
-depends "build-essential"
 depends "nginx"
 depends "thin"
