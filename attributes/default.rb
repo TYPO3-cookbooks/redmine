88af92df12112670ab1f02685a75c4d854b4f14f
Peter Niederlag <netservice@niekom.de>
2013-04-17 11:56:59 +0200

    change default for deploy_to to /srv/redmine

--- attributes/default.rb
+++ attributes/default.rb
@@ -26,7 +26,7 @@ end
 default['redmine']['rails_env'] = "production"
 default['redmine']['thin_servers'] = "1"
 
-default['redmine']['deploy_to'] = "/usr/share/redmine"
+default['redmine']['deploy_to'] = "/srv/redmine"
 
 default['redmine']['hostname'] = fqdn
 
