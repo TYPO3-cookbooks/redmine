602e9804dd1b34f482fa2f7097c780d3cbbdb014
Peter Niederlag <peter.niederlag@typo3.org>
2014-10-01 18:11:01 +0200

    adapt log directory

--- recipes/logrotate.rb
+++ recipes/logrotate.rb
@@ -1,6 +1,6 @@
 logrotate_app 'redmine' do
   cookbook  'logrotate'
-  path      ["#{node['redmine']['deploy_to']}/shared/*.log"]
+  path      ["#{node['redmine']['deploy_to']}/shared/log/*.log"]
   frequency 'weekly'
   rotate    12
   options ["copytruncate"]
