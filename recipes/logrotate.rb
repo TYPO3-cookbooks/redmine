7dfd67ea88c407ac59fab0a4d6205876b55ad7ec
Steffen Gebert <steffen.gebert@typo3.org>
2015-01-18 16:06:12 +0100

    Use daily rogrotate

--- recipes/logrotate.rb
+++ recipes/logrotate.rb
@@ -1,7 +1,7 @@
 logrotate_app 'redmine' do
   cookbook  'logrotate'
   path      ["#{node['redmine']['deploy_to']}/shared/log/*.log"]
-  frequency 'weekly'
-  rotate    12
+  frequency 'daily'
+  rotate    30
   options ["copytruncate", "compress", "delaycompress", "notifempty", "dateext"]
 end
